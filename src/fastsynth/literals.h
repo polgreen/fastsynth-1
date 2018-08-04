62cb796f0d9c0de996b3b6fc77823cba682a3e42
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2018-08-04 12:18:17 +0200

    tweak literals

--- src/fastsynth/literals.h
+++ src/fastsynth/literals.h
@@ -1,8 +1,17 @@
 #ifndef CPROVER_FASTSYNTH_LITERALS_H_
 #define CPROVER_FASTSYNTH_LITERALS_H_
 
+#include <util/std_expr.h>
+#include <set>
 /// Extends a problem by helper literals.
 /// \param problem Problem to extend and in which to search literals.
 void add_literals(class problemt &problem);
+
+
+/// Collects all constant literals explicitly used in the problem description.
+/// \param problem Problem in which to search for literals.
+/// \return All constant literals located in the given problem.
+std::set<exprt> find_literals(const class problemt &problem);
+
 
 #endif /* CPROVER_FASTSYNTH_LITERALS_H_ */
