09858761c41c82a0732ab3ee620c1a50cd7ec7bc
polgreen <epolgreen@gmail.com>
2020-09-25 17:30:27 -0700

    reformatting c frntend

--- src/fastsynth/c_frontend.cpp
+++ src/fastsynth/c_frontend.cpp
@@ -41,12 +41,12 @@ std::set<irep_idt> find_expressions(const goto_modelt &goto_model)
 {
   std::set<irep_idt> result;
 
-  for(auto &s : goto_model.symbol_table.symbols)
+  for (auto &s : goto_model.symbol_table.symbols)
   {
-    if(!s.second.is_state_var &&
-       s.second.type.id()==ID_code &&
-       s.second.value.is_nil() &&
-       has_prefix(id2string(s.first), "EXPRESSION"))
+    if (!s.second.is_state_var &&
+        s.second.type.id() == ID_code &&
+        s.second.value.is_nil() &&
+        has_prefix(id2string(s.first), "EXPRESSION"))
       result.insert(s.first);
   }
 
@@ -54,54 +54,54 @@ std::set<irep_idt> find_expressions(const goto_modelt &goto_model)
 }
 
 void instrument_expressions(
-  const std::set<irep_idt> &expressions,
-  goto_modelt &goto_model)
+    const std::set<irep_idt> &expressions,
+    goto_modelt &goto_model)
 {
-  for(auto &f : goto_model.goto_functions.function_map)
-    for(auto &i : f.second.body.instructions)
+  for (auto &f : goto_model.goto_functions.function_map)
+    for (auto &i : f.second.body.instructions)
     {
-      if(i.is_function_call())
+      if (i.is_function_call())
       {
-        auto &c=to_code_function_call(i.code);
-        if(c.function().id()==ID_symbol)
+        auto &c = to_code_function_call(i.code);
+        if (c.function().id() == ID_symbol)
         {
-          irep_idt identifier=to_symbol_expr(c.function()).get_identifier();
-          if(expressions.find(identifier)!=expressions.end() &&
-             c.lhs().is_not_nil())
+          irep_idt identifier = to_symbol_expr(c.function()).get_identifier();
+          if (expressions.find(identifier) != expressions.end() &&
+              c.lhs().is_not_nil())
           {
             code_typet code_type = to_code_type(c.function().type());
             const typet &codomain = code_type.return_type();
             code_typet::parameterst &params = code_type.parameters();
             mathematical_function_typet::domaint domain;
 
-            for(const auto &p : code_type.parameters())
+            for (const auto &p : code_type.parameters())
             {
-              if(p.type().id()==ID_pointer)
+              if (p.type().id() == ID_pointer)
               {
-                typet type=array_typet(p.type().subtype(),
-                    symbol_exprt(p.id(), p.type().subtype()));
+                typet type = array_typet(p.type().subtype(),
+                                         symbol_exprt(p.id(), p.type().subtype()));
                 domain.push_back(type);
               }
               else
                 domain.push_back(p.type());
             }
-            assert(params.size()==domain.size());
+            assert(params.size() == domain.size());
 
             const mathematical_function_typet type(domain, codomain);
 
-            i.type=ASSIGN;
+            i.type = ASSIGN;
 
-                       // replace pointer to array arguments with arrays
+            //replace pointer to array arguments with arrays
             exprt::operandst new_arguments;
-            for(auto &arg : c.arguments())
-             {
-              if(arg.id() == ID_address_of)
+            for (auto &arg : c.arguments())
+            {
+              if (arg.id() == ID_address_of)
               {
                 address_of_exprt tmp = to_address_of_expr(arg);
-                if(tmp.object().id() == ID_index)
+                if (tmp.object().id() == ID_index)
                 {
-                  std::cout <<"Replacing ID address of with array\n";
-                  exprt new_expr=to_index_expr(tmp.object()).array();
+                  std::cout << "Replacing ID address of with array\n";
+                  exprt new_expr = to_index_expr(tmp.object()).array();
                   new_arguments.push_back(new_expr);
                 }
                 else
@@ -110,15 +110,14 @@ void instrument_expressions(
               else
                 new_arguments.push_back(arg);
             }
-           assert(new_arguments.size()==c.arguments().size());
-
+            assert(new_arguments.size() == c.arguments().size());
 
             function_application_exprt e(
-              symbol_exprt(identifier, type),
-              c.arguments(),
-              c.lhs().type());
+                symbol_exprt(identifier, type),
+                new_arguments,
+                c.lhs().type());
 
-            i.code=code_assignt(c.lhs(), e);
+            i.code = code_assignt(c.lhs(), e);
           }
         }
       }
@@ -126,17 +125,17 @@ void instrument_expressions(
 }
 
 void show_formula(
-  const symex_target_equationt &src,
-  const namespacet &ns)
+    const symex_target_equationt &src,
+    const namespacet &ns)
 {
-  for(const auto &step : src.SSA_steps)
+  for (const auto &step : src.SSA_steps)
   {
-    if(step.is_assignment() ||
-       step.is_assume())
+    if (step.is_assignment() ||
+        step.is_assume())
     {
       std::cout << "P: " << from_expr(ns, "", step.cond_expr) << '\n';
     }
-    else if(step.is_assert())
+    else if (step.is_assert())
     {
       std::cout << "A: " << from_expr(ns, "", step.cond_expr) << '\n';
     }
@@ -149,14 +148,15 @@ int c_frontend(const cmdlinet &cmdline)
   messaget message(mh);
 
   // this is our default verbosity
-  unsigned int v=messaget::M_STATISTICS;
+  unsigned int v = messaget::M_STATISTICS;
 
-  if(cmdline.isset("verbosity"))
+  if (cmdline.isset("verbosity"))
   {
-    v=std::stol(
-        cmdline.get_value("verbosity"));;
-    if(v>10)
-      v=10;
+    v = std::stol(
+        cmdline.get_value("verbosity"));
+    ;
+    if (v > 10)
+      v = 10;
   }
 
   mh.set_verbosity(v);
@@ -166,23 +166,23 @@ int c_frontend(const cmdlinet &cmdline)
   config.set(cmdline);
   config.ansi_c.set_arch_spec_i386();
 
-  PRECONDITION(cmdline.args.size()==1);
+  PRECONDITION(cmdline.args.size() == 1);
 
   goto_modelt goto_model;
 
   try
   {
     optionst options;
-    goto_model=initialize_goto_model(cmdline.args, mh, options);
+    goto_model = initialize_goto_model(cmdline.args, mh, options);
   }
-  catch(...)
+  catch (...)
   {
     return 1;
   }
 
-  auto expressions=find_expressions(goto_model);
+  auto expressions = find_expressions(goto_model);
 
-  for(const auto &i : expressions)
+  for (const auto &i : expressions)
     message.status() << "EXPRESSION: " << i << messaget::eom;
 
   instrument_expressions(expressions, goto_model);
@@ -194,7 +194,7 @@ int c_frontend(const cmdlinet &cmdline)
   symbol_tablet new_symbol_table;
   namespacet ns(goto_model.symbol_table, new_symbol_table);
   symex_target_equationt equation(mh);
-  auto path_storage=get_path_strategy("lifo");
+  auto path_storage = get_path_strategy("lifo");
   guard_managert guard_manager;
   optionst options;
 
@@ -202,52 +202,50 @@ int c_frontend(const cmdlinet &cmdline)
   options.set_option("simplify", true);
 
   goto_symext goto_symex(
-    mh,
-    goto_model.symbol_table,
-    equation,
-    options,
-    *path_storage,
-    guard_manager);
+      mh,
+      goto_model.symbol_table,
+      equation,
+      options,
+      *path_storage,
+      guard_manager);
 
-  auto get_goto_function = [&goto_model](const irep_idt &id) ->
-    const goto_functionst::goto_functiont &
-    {
-      return goto_model.get_goto_function(id);
-    };
+  auto get_goto_function = [&goto_model](const irep_idt &id) -> const goto_functionst::goto_functiont & {
+    return goto_model.get_goto_function(id);
+  };
 
   goto_symex.symex_from_entry_point_of(
-    get_goto_function,
-    new_symbol_table);
+      get_goto_function,
+      new_symbol_table);
 
-  #if 0
+#if 0
   show_formula(equation, ns);
-  #endif
+#endif
 
   problemt problem = to_problem(mh, options, goto_model);
-  if(cmdline.isset("literals"))
+  if (cmdline.isset("literals"))
     add_literals(problem);
 
   cegist cegis(ns);
   cegis.set_message_handler(mh);
 
-  if(cmdline.isset("max-program-size"))
-    cegis.max_program_size=std::stol(
-      cmdline.get_value("max-program-size"));
+  if (cmdline.isset("max-program-size"))
+    cegis.max_program_size = std::stol(
+        cmdline.get_value("max-program-size"));
   else
-    cegis.max_program_size=5; // default
+    cegis.max_program_size = 5; // default
 
-  cegis.enable_bitwise=!cmdline.isset("no-bitwise");\
-  cegis.use_smt=cmdline.isset("smt");
-  cegis.enable_division=cmdline.isset("enable-division");
-  cegis.logic="BV"; //default logic
+  cegis.enable_bitwise = !cmdline.isset("no-bitwise");
+  cegis.use_smt = cmdline.isset("smt");
+  cegis.enable_division = cmdline.isset("enable-division");
+  cegis.logic = "BV"; //default logic
 
-  auto start_time=std::chrono::steady_clock::now();
+  auto start_time = std::chrono::steady_clock::now();
 
-  switch(cegis(problem))
+  switch (cegis(problem))
   {
   case decision_proceduret::resultt::D_SATISFIABLE:
 
-    for(const auto &f : cegis.solution.functions)
+    for (const auto &f : cegis.solution.functions)
     {
       message.result() << "Result: "
                        << f.first.get_identifier()
@@ -260,7 +258,8 @@ int c_frontend(const cmdlinet &cmdline)
 
     message.statistics() << "Synthesis time: "
                          << std::chrono::duration<double>(
-                              std::chrono::steady_clock::now()-start_time).count()
+                                std::chrono::steady_clock::now() - start_time)
+                                .count()
                          << 's'
                          << messaget::eom;
     break;
